2153fa494289fe2bb1ca31fcb2cb1a233489c180
user <ian.slippers@ingenico.com>
2022-06-09 12:53:09 +0200

    add tests

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/worldline-go/exodus
 
 go 1.18
+
+require github.com/matryer/is v1.4.0
--- slices/slices.go
+++ slices/slices.go
@@ -13,40 +13,8 @@ func Concat[S ~[]T, T any](slices ...S) S {
 	return result
 }
 
-// Product returns cortesian product of elements
-// {{1, 2}, {3, 4}} -> {1, 3}, {1, 4}, {2, 3}, {2, 4}
-func Product2[T any](items ...[]T) chan []T {
-	c := make(chan []T, 1)
-	go product2(items, c, []T{}, 0)
-	return c
-}
-
-// product is a core implementation of Product
-func product2[T any](items [][]T, c chan []T, left []T, pos int) {
-	// iterate over the last array
-	if pos == len(items)-1 {
-		for _, el := range items[pos] {
-			result := make([]T, 0, len(left)+1)
-			result = append(result, left...)
-			result = append(result, el)
-			c <- result
-		}
-		return
-	}
-
-	for _, el := range items[pos] {
-		result := make([]T, 0, len(left)+1)
-		result = append(result, left...)
-		result = append(result, el)
-		product2(items, c, result, pos+1)
-	}
-
-	if pos == 0 {
-		close(c)
-	}
-}
-
-// Zip returns chan of arrays of elements from given arrs on the same position.
+// Zip returns chan of arrays of elements
+// from given arrays on the same position.
 func Zip[S ~[]T, T any](items ...S) chan S {
 	if len(items) == 0 {
 		result := make(chan S)
